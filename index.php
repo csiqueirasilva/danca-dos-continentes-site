ac61e4dd2324899537fcb2239b5f7ae62c66df79
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2017-01-04 14:06:36 -0200

    initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+﻿<html>
+	<head>
+		<title>Dança dos Continentes</title>
+		<script src="jquery.min.js"></script>
+		<link href="style.css" rel="stylesheet" />
+	</head>
+	<body>
+	
+		<div id="iframe-wrapper">
+			<iframe height="100%" width="" frameborder="0" src="danca-dos-continentes/src/index.html"></iframe>
+			<img src="question.png" id="question-sign" />
+		</div>
+		
+		<img src="info.png" id="text-content" />
+		
+		<img src="arrow-circle-top.png" id="arrow-circle-top" style="display: none; "/>
+		
+		<script>
+		
+			$(window).scroll(function() {
+				if(document.body.scrollTop < window.innerHeight * 0.01) {
+					$('#arrow-circle-top').hide();
+					$('#question-sign').show();
+				} else {
+					$('#arrow-circle-top').show();
+					$('#question-sign').hide();
+				}
+			});
+		
+			$('#question-sign').click(function() {
+				$("html, body").animate({ scrollTop: $('#text-content').offset().top }, 1000);
+			});
+		
+			$('#arrow-circle-top').click(function() {
+				$("html, body").animate({ scrollTop: 0 }, 1000);
+			});
+		
+			function resizeIframe () {
+				var iframe = $('iframe');
+				var height = iframe.height();
+				var width = parseInt(height * ( 16 / 9 ));
+				iframe.width(width);
+			}
+		
+			$('iframe').mouseover(function(ev) {
+				document.body.style.overflow = 'hidden';
+			});
+
+			$('iframe').mouseout(function(ev) {
+				document.body.style.overflow = 'auto';
+			});
+			
+			resizeIframe();
+		
+			$('window').trigger('resize', resizeIframe);
+			
+		</script>
+	</body>
+</html>
